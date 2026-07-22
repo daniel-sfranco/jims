aabfa25966f110c3b9b34dd051cc8528c67bddc5
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-07-22 14:00:23 -0300

    Creating command init, which only creates the files and folders for now

---
+++ src/init.rs
@@ -0,0 +1,66 @@
+use std::{fs, path::Path};
+
+#[derive(Debug)]
+pub enum InitError {
+    OS(String),
+}
+
+impl InitError {
+    fn from_path(path: &str, message: String) -> Self {
+        Self::OS(format!("Error creating the path {path}: {message}"))
+    }
+
+}
+
+fn create_folder(folder_name: &str) -> Result<(), InitError> {
+    let creation_result = fs::create_dir_all(folder_name);
+    if let Err(_) = creation_result {
+        return Err(InitError::from_path(folder_name, creation_result.unwrap_err().to_string()))
+    }
+    Ok(())
+}
+
+
+fn create_file(file: (&str, &str)) -> Result<(), InitError> {
+    let creation_result = fs::write(file.0, file.1);
+    if let Err(_) = creation_result {
+        return Err(InitError::from_path(file.0, creation_result.unwrap_err().to_string()))
+    }
+    Ok(())
+}
+
+
+pub fn init() -> Result<(), InitError> {
+    let base_folder = Path::new(".jims");
+
+    let subfolders = vec!["objects", "refs"];
+    for sub in subfolders {
+        let joined_path = base_folder.join(sub);
+        let path = joined_path.to_str();
+        if let None = path {
+            return Err(InitError::from_path(sub, String::from("Problem creating the path for this folder")))
+        }
+        let creation_result = create_folder(path.unwrap());
+        if let Err(err) = creation_result {
+            return Err(err)
+        }
+    }
+
+    let files: Vec<(&str, &str)> = vec![
+        ("HEAD", "ref: refs/heads/master\n"), 
+        ("description", "Unnamed repository; edit this file 'description' to name the repository\n"),
+        ("config", "[core]\n    repositoryformatversion = 0\n    filemode = false\n    bare = false\n")
+    ];
+    for file in files {
+        let joined_path = base_folder.join(file.0);
+        let path = joined_path.to_str();
+        if let None = path {
+            return Err(InitError::from_path(file.0, String::from("Problem creating the path for this file")))
+        }
+        let creation_result = create_file((path.unwrap(), file.1));
+        if let Err(err) = creation_result {
+            return Err(err)
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
 
+mod init;
+
 fn main() {
-    let args:Vec<String> = env::args().collect();
-    dbg!(args);
+    let args: Vec<String> = env::args().collect();
+    // args.push(String::from("init")); // just for vs code debug
+    if args.len() < 2 {
+        panic!("At least 1 argument are necessary \nHelp: without a query I don't know what to do!");
+    }
+    let query = args[1].as_str();
+    let output = match query {
+        "init" => init::init(),
+        _ => panic!("Query not recognized! Try one of the following: init"),
+    };
+    if let Err(init::InitError::OS(error)) = &output {
+        println!("There was an error: {error}");
+    }
+    println!("{query} resulted in: {output:?}");
 }
