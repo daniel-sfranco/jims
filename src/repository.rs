af9334a954db8114ac23bd37cb82c2f424dfa403
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-07-23 13:27:31 -0300

    Creating the repo_find function

--- src/repository.rs
+++ src/repository.rs
@@ -1,17 +1,17 @@
-use std::{path, error::Error};
+use std::{path::PathBuf, error::Error};
 use configparser::ini;
 
 use crate::init::InitError;
 
 #[derive(Debug)]
 pub struct Repository {
-    pub dir: path::PathBuf,
-    pub worktree: path::PathBuf,
+    pub dir: PathBuf,
+    pub worktree: PathBuf,
     pub conf: ini::Ini,
 }
 
 impl Repository {
-    pub fn new(path: path::PathBuf) -> Result<Self, Box<dyn Error>> {
+    pub fn new(path: PathBuf) -> Result<Self, Box<dyn Error>> {
         let path_copy = path.clone();
         let jims_dir = path_copy.join(".jims");
         if !jims_dir.exists() || !jims_dir.is_dir() {
@@ -25,4 +25,27 @@ impl Repository {
         repo.conf.load(repo.dir.join("config"))?;
         Ok(repo)
     }
+}
+
+pub fn repo_find(path: PathBuf, required: bool) -> Result<Option<PathBuf>, String> {
+    let mut path = path.clone();
+    if path.is_relative() {
+        path = path.canonicalize().unwrap();
+    }
+
+    let jims = path.join(".jims");
+    if jims.exists() && jims.is_dir() {
+        return Ok(Some(path))
+    }
+
+    let parent = path.join("..");
+    if parent == path {
+        if required {
+            return Err("Not a git directory".to_string())
+        } else {
+            return Ok(None)
+        }
+    }
+
+    repo_find(parent, required)
 }
